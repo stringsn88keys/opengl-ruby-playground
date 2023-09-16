6db9cb4239a1ee6706adf171f042de0119dd8caf
Thomas Powell <twilliampowell@gmail.com>
2023-09-16 09:30:08 -0400

    Two faces of a cube
    Signed-off-by: Thomas Powell <twilliampowell@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # opengl-ruby-playground
 Ruby scripts with OpenGL
+
+# Use
+Start with [ruby-opengl](https://github.com/vaiorabbit/ruby-opengl.git)
+
+You may also need [lib-glfw](https://www.glfw.org/)
+
+## Manjaro
+
+
+```sh
+sudo pacman -S glfw
+```
+```txt
+sample/util/setup_dll.rb:      '/usr/lib/x86_64-linux-gnulibGL.so'
+sample/util/setup_dll.rb:      '/usr/lib/x86_64-linux-gnu/libGLU.so'
+sample/util/setup_dll.rb:      '/usr/lib/x86_64-linux-gnu/libglfw.so'`
+```
+
+
+[GLFW download](https://www.glfw.org/download.html)
+[Unity OpneGL reference](https://docs.unity3d.com/ScriptReference/GL.TRIANGLES.html)
+[compiling glfw](https://www.glfw.org/docs/latest/compile.html)
+
+```sh
+cmake -S path/to/glfw -B path/to/build -D BUILD_SHARED_LIBS=ON
+cd path/to/build
+make
+``
+
+```sh
+ruby -Ipath/to/ruby-opengl/lib playground_script_to_run.rb
+```
---
+++ simple.rb
@@ -0,0 +1,71 @@
+#
+# For more samples, visit https://github.com/vaiorabbit/ruby-opengl/tree/master/sample .
+#
+# Ref.: /glfw-3.0.1/examples/simple.c
+#
+require 'opengl'
+require 'glfw'
+
+# Press ESC to exit.
+key_callback = GLFW::create_callback(:GLFWkeyfun) do |window, key, scancode, action, mods|
+  GLFW.SetWindowShouldClose(window, 1) if key == GLFW::KEY_ESCAPE && action == GLFW::PRESS
+end
+
+if __FILE__ == $PROGRAM_NAME
+  GLFW.load_lib() # Give path to "glfw3.dll (Windows)" or "libglfw.dylib (macOS)" if needed
+  GLFW.Init()
+
+  window = GLFW.CreateWindow(640, 480, "Simple example", nil, nil)
+  GLFW.MakeContextCurrent(window)
+  GLFW.SetKeyCallback(window, key_callback)
+
+  GL.load_lib() # Call GL.load_lib after OpenGL context is available
+
+  width_buf = ' ' * 8
+  height_buf = ' ' * 8
+  until GLFW.WindowShouldClose(window) == GLFW::TRUE
+    GLFW.GetFramebufferSize(window, width_buf, height_buf)
+    width = width_buf.unpack1('L')
+    height = height_buf.unpack1('L')
+    ratio = width.to_f / height.to_f
+
+    GL.Viewport(0, 0, width, height)
+    GL.Clear(GL::COLOR_BUFFER_BIT)
+    GL.MatrixMode(GL::PROJECTION)
+    GL.LoadIdentity()
+    GL.Ortho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
+    GL.MatrixMode(GL::MODELVIEW)
+
+    GL.LoadIdentity()
+    GL.Rotatef(GLFW.GetTime() * 50.0, GLFW.GetTime*25.0, GLFW.GetTime*12.5, 1.0)
+
+=begin
+    GL.Begin(GL::TRIANGLES)
+    GL.Color3f(1.0, 0.0, 0.0)
+    GL.Vertex3f(-0.6, -0.4, 0.0)
+    GL.Color3f(0.0, 1.0, 0.0)
+    GL.Vertex3f(0.6, -0.4, 0.0)
+    GL.Color3f(0.0, 0.0, 1.0)
+    GL.Vertex3f(0.0, 0.6, 0.0)
+    GL.End()
+=end
+    GL.Begin(GL::QUADS)
+    [-1,1].each do |z|
+      [
+        [1,1,z],
+        [-1,1,z],
+        [-1,-1,z],
+        [1,-1,z]
+      ].each do |point|
+        GL.Color3f(point[0]*2+1, point[1]*2+1, point[2]*2+1)
+        GL.Vertex3f(point[0]*0.5, point[1]*0.5, point[2]*0.5)
+      end
+    end
+    GL.End()
+    GLFW.SwapBuffers(window)
+    GLFW.PollEvents()
+  end
+
+  GLFW.DestroyWindow(window)
+  GLFW.Terminate()
+end
