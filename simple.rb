4b5a8783cea9b9002d4720bed66527f24f285406
Thomas Powell <twilliampowell@gmail.com>
2023-09-16 11:01:10 -0400

    Working cube demo
    Signed-off-by: Thomas Powell <twilliampowell@gmail.com>

--- simple.rb
+++ simple.rb
@@ -10,9 +10,8 @@ require 'glfw'
 key_callback = GLFW::create_callback(:GLFWkeyfun) do |window, key, scancode, action, mods|
   GLFW.SetWindowShouldClose(window, 1) if key == GLFW::KEY_ESCAPE && action == GLFW::PRESS
 end
-
 if __FILE__ == $PROGRAM_NAME
-  GLFW.load_lib() # Give path to "glfw3.dll (Windows)" or "libglfw.dylib (macOS)" if needed
+  GLFW.load_lib(File.join(ENV['LD_LIBRARY_PATH'], 'libglfw.dylib')) # Give path to "glfw3.dll (Windows)" or "libglfw.dylib (macOS)" if needed
   GLFW.Init()
 
   window = GLFW.CreateWindow(640, 480, "Simple example", nil, nil)
@@ -30,38 +29,39 @@ if __FILE__ == $PROGRAM_NAME
     ratio = width.to_f / height.to_f
 
     GL.Viewport(0, 0, width, height)
-    GL.Clear(GL::COLOR_BUFFER_BIT)
+    # http://www.opengl-tutorial.org/beginners-tutorials/tutorial-4-a-colored-cube/
+    # need to clear both color buffer and depth buffer
+    GL.Clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT)
     GL.MatrixMode(GL::PROJECTION)
     GL.LoadIdentity()
     GL.Ortho(-ratio, ratio, -1.0, 1.0, 1.0, -1.0)
     GL.MatrixMode(GL::MODELVIEW)
 
+    # need to have this in order to not just draw in order of shape definition
+    GL.Enable(GL::DEPTH_TEST)
+    GL.DepthFunc(GL::LESS)
+
     GL.LoadIdentity()
     GL.Rotatef(GLFW.GetTime() * 50.0, GLFW.GetTime*25.0, GLFW.GetTime*12.5, 1.0)
 
-=begin
-    GL.Begin(GL::TRIANGLES)
-    GL.Color3f(1.0, 0.0, 0.0)
-    GL.Vertex3f(-0.6, -0.4, 0.0)
-    GL.Color3f(0.0, 1.0, 0.0)
-    GL.Vertex3f(0.6, -0.4, 0.0)
-    GL.Color3f(0.0, 0.0, 1.0)
-    GL.Vertex3f(0.0, 0.6, 0.0)
-    GL.End()
-=end
-    GL.Begin(GL::QUADS)
-    [-1,1].each do |z|
-      [
-        [1,1,z],
-        [-1,1,z],
-        [-1,-1,z],
-        [1,-1,z]
-      ].each do |point|
-        GL.Color3f(point[0]*2+1, point[1]*2+1, point[2]*2+1)
-        GL.Vertex3f(point[0]*0.5, point[1]*0.5, point[2]*0.5)
+    [0,1,2].each do |rot|
+      [-1,1].each do |plane|
+        color_vector=[127+plane*63,0,0].rotate(rot)
+        GL.Begin(GL::QUADS)
+        [
+          [1,1,plane],
+          [1,-1,plane],
+          [-1,-1,plane],
+          [-1,1,plane],
+        ].map { |vector| vector.rotate(rot) }.each do |point|
+#          GL.Color3f(*color_vector)
+          GL.Color3f(point[0]*0.5+0.5, point[1]*0.5+0.5, point[2]*0.5+0.5)
+          GL.Vertex3f(point[0]*0.5, point[1]*0.5, point[2]*0.5)
+        end
+        GL.End()
+        GL.Flush()
       end
     end
-    GL.End()
     GLFW.SwapBuffers(window)
     GLFW.PollEvents()
   end
